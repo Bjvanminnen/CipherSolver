fa0072ea8f60c77bc83d3f53271e83f5a7d4cf25
Brent Van Minnen <bjvanminnen@gmail.com>
2017-02-26 23:56:47 -0800

    let flexbox take care of wrapping

--- src/CipherCardSet.js
+++ src/CipherCardSet.js
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 
 const styles = {
-  row: {
-    flexDirection: 'row'
-  }
+  main: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    // TODO - would like to center this, but I don't want the last row centered
+    // will need to figure out how to accomplish that
+    // justifyContent: 'center'
+  },
 };
 
-const CipherCardSet = ({ characters, cardsPerRow, CardComponent }) => {
-
-  let ungrouped = characters.split('');
-  const grouped = [];
-  while (ungrouped.length) {
-    grouped.push(ungrouped.splice(0, cardsPerRow));
-  }
-  return (
-    <View>
-      {grouped.map((group, groupKey) => (
-        <View
-          key={groupKey}
-          style={styles.row}
-        >
-          {group.map((char, charKey) => (
-            <CardComponent
-              key={charKey}
-              character={char}
-            />
-          ))}
-        </View>
-      ))}
-    </View>
-  );
-};
+const CipherCardSet = ({ characters, CardComponent }) => (
+  <View style={styles.main}>
+    {characters.split('').map((char, index) => (
+      <CardComponent
+        key={index}
+        character={char}
+      />
+    ))}
+  </View>
+);
 
 export default CipherCardSet;
